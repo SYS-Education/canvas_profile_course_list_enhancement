ee283fcaa8e3a146f59fc650958b1282f1b6994a
liambeveridge <32782502+liambeveridge@users.noreply.github.com>
2021-12-14 16:46:12 -0800

    initial commit

---
+++ canvas_profile_course_list_enhancement.js
@@ -0,0 +1,24 @@
+/*
+The following code re-roders the "Courses" list in the "Enrollments" section on the admin user profile page in Canvas.
+The list of courses is re-ordered so that active courses will be placed at the top of the list, while preserving the alphabetical order that
+the active courses would were listed in. All non-active courses are left in their original order below the active courses.
+*/
+function reorder_profile_course_list() {
+    let currentPage = window.location.pathname;
+    if(currentPage.includes("/users/") && !currentPage.includes("user_notes") && !currentPage.includes("courses")){
+        var list = document.getElementsByClassName("unstyled_list context_list")[0];
+        // Optional line to double the height of the courses list:
+        // list.style.maxHeight = "400px"
+        var items = list.getElementsByTagName('li');
+        var stop = 0;
+        index = items.length - 1;
+        while(index > stop) {
+            if(items[index].classList.contains("active")){
+                items[index].parentNode.insertBefore(items[index], items[0]);
+                stop++;
+            } else { 
+                index--;
+            }
+        }
+    }
+  }
